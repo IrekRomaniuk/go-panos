913052e6b189a8339a58ad908c238793b1d4946d
irom77 <irek.romaniuk@gmail.com>
2017-03-22 10:42:36 -0400

    added debug to CreateAddress method

--- address.go
+++ address.go
@@ -196,7 +196,7 @@ func (p *PaloAlto) CreateAddress(name, addrtype, address, description string, sh
 	if errs != nil {
 		return errs[0]
 	} else {
-		fmt.Println(resp)
+		fmt.Println("\n",resp)
 	}
 
 	if err := xml.Unmarshal([]byte(resp), &reqError); err != nil {
